c1e79c967dbd830e28a3fc93333a9e0c4b65ff47
Mircea Lupan <mlupan@yahoo.co.uk>
2022-01-18 01:48:21 +0200

    Add Pagination to Blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 2)
+    page = request.GET.get("page")
+    posts = paginator.get_page(page)
+    return render(request, "blog/post/list.html", {"page": page, "posts": posts})
 
 
 def post_detail(request, year, month, day, post):
