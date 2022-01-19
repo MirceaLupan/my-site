c04919b4a306692842ce4682b6bbf9444e62efc1
Mircea Lupan <mlupan@yahoo.co.uk>
2022-01-19 23:48:05 +0200

    Add the tagging functionality

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,25 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.views.generic import ListView
+from taggit.models import Tag
+from django.db.models import Count
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 2
-    template_name = "blog/post/list.html"
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 2)
+    page = request.GET.get("page")
+    posts = paginator.get_page(page)
+    return render(
+        request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag}
+    )
 
 
 def post_detail(request, year, month, day, post):
@@ -36,6 +46,12 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
     return render(
         request,
         "blog/post/detail.html",
@@ -44,6 +60,7 @@ def post_detail(request, year, month, day, post):
             "comments": comments,
             "new_comment": new_comment,
             "comment_form": comment_form,
+            "similar_posts": similar_posts,
         },
     )
 
